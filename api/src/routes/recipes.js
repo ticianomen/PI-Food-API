ec77df3a555eb88fb465e92a490639877a5b2880
ticianomen <ticianomensegue@hotmail.com>
2021-07-14 00:04:04 -0300

    backend almost done

--- api/src/routes/recipes.js
+++ api/src/routes/recipes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const { Recipe } = require('../db.js');
+const { Recipe, Diet } = require('../db.js');
 const fetch = require('node-fetch')
+const {Op} = require('sequelize')
 require('dotenv').config()
 
 const{ MY_API_KEY } = process.env;
@@ -12,20 +13,31 @@ const{ MY_API_KEY } = process.env;
 
 router.get('/recipes/:id', async function(req,res){
     const { id } = req.params;
-    const response = await fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${MY_API_KEY}`);
-    const recipe = await response.json();
-
-    let data = {
-        image : recipe.image,
-        title : recipe.title,
-        dishTypes : recipe.dishTypes,
-        diets : recipe.diets,
-        summary : recipe.summary,
-        spoonacularScore : recipe.spoonacularScore,
-        healthScore : recipe.healthScore,
-        instructions : recipe.instructions,
+    try{
+        const dietDB = await Recipe.findByPk(id,{include: Diet})
+        res.json(dietDB)
     }
-    res.status(200).json(data)
+    catch{
+        try{
+            const response = await fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${MY_API_KEY}`);
+            const recipe = await response.json();
+            let data = {
+                image : recipe.image,
+                title : recipe.title,
+                dishTypes : recipe.dishTypes,
+                diets : recipe.diets,
+                summary : recipe.summary,
+                spoonacularScore : recipe.spoonacularScore,
+                healthScore : recipe.healthScore,
+                instructions : recipe.instructions,
+            }
+            res.status(200).json(data)
+        }
+        catch{
+            res.status(404).send("An error ocurred")
+        }
+    }
+    
 });
 
 // GET /recipes?name="...":
@@ -34,12 +46,38 @@ router.get('/recipes/:id', async function(req,res){
 
 router.get('/recipes', async function(req,res){
     const{ name } = req.query
-    const response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?titleMatch=${name}&number=9&apiKey=${MY_API_KEY}`)
-    if(response.length<1){
-        res.status(404).send("No hay recetas que coincidan con ese nombre")
+    if(name){
+        let recipesDB = await Recipe.findAll({
+            limit: 9,
+            where: { 
+                title : {
+                    [Op.substring]: name
+                } 
+            },
+        })
+        if(recipesDB.length>8){
+            res.json(recipesDB)
+        }
+        else{
+            let numbers= 9 - recipesDB.length
+            let response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?titleMatch=${name}&number=${numbers}&apiKey=${MY_API_KEY}`)
+            response = await response.json()
+            if(recipesDB.length<1 && response.totalResults===0){
+                res.status(404).send("No hay recetas que coincidan con ese nombre")
+            }else{
+                res.json(recipesDB.concat(response.results))
+            }
+        }
     }else{
-        const data= await response.json()
-        res.json(data)
+        let recipesDB = await Recipe.findAll({ limit: 100 })
+        if(recipesDB.length>99){
+            res.json(recipesDB)
+        }else{
+            let numbers= 100 - recipesDB.length
+            let response = await fetch(`https://api.spoonacular.com/recipes/complexSearch?number=${numbers}&apiKey=${MY_API_KEY}`)
+            response = await response.json()
+            res.json(recipesDB.concat(response.results))
+        }
     }
 });
 
@@ -48,8 +86,9 @@ router.get('/recipes', async function(req,res){
 // Crea una receta en la base de datos
 
 router.post('/recipe', async function(req,res){
-    const {title,summary,spoonacularScore,healthScore,instructions} = req.body
+    const {title,summary,spoonacularScore,healthScore,instructions,diets} = req.body
     let recipe = await Recipe.create({title,summary,spoonacularScore,healthScore,instructions})
+    recipe.setDiets(diets)
     return res.status(200).json(recipe)
 });
 
