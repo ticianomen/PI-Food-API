39718272462008d5e9ba9b74fc27059ab6110078
Ticiano Yoel Mensegue <83244923+ticianomen@users.noreply.github.com>
2021-09-08 19:29:14 -0300

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -16,6 +16,7 @@ server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Headers', "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
