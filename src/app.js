08a3827038e161f4f6809cdda9abad5ab2ad2e40
Ticiano <ticianomensegue@hotmaiil.com>
2021-09-10 03:48:56 -0300

    deleted /api and created .env

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://food-app-ticiano.herokuapp.com");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
